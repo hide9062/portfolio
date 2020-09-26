1e688ba0f534b6c26d8a1b8cbc8345a5beef0aba
hede9062 <hide@hidenoMacBook-Air.local>
2020-09-26 20:38:19 +0900

    ポートフォリオ

---
+++ README.md
@@ -0,0 +1,6 @@
+`sass`(`scss`記法)と`javascript`の勉強用として、  
+架空のcafeサイトを作りました。  
+レスポンシブ対応しています。  
+（フロント側の勉強用で作成したものになるのでフォームは機能いたしません。）  
+
+file:///Users/hide/Desktop/portfolio/index.html#about
---
+++ js/app.js
@@ -0,0 +1,14 @@
+AOS.init({
+  duration: 1100,
+  easing: 'easeOutSine'
+});
+
+$(function() {
+  $('a[href^="#"]').click(function () {
+    var time = 600;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html,body").animate({ scrollTop: position }, time, "swing");
+    return false;
+  });
